30020a58903f02ab8ae1c9cee49701bcab0a0d91
RobertoCastro <robertoca07@hotmail.com>
2023-11-11 18:37:46 -0500

    Data file added

---
+++ week1/statistic_project/data/data_array.h
@@ -0,0 +1,29 @@
+/********************************************************************************
+ * @file data_array.h
+ * @brief Data to perform calculations
+ *
+ * This file contains the data matrices to be used to calculate its 
+ * statistical parameters.
+ *
+ * @author Roberto Castro Beltran
+ * @date 08 november 2023
+ *
+ *******************************************************************************/
+
+#ifndef _DATA_ARRAY_H_
+#define _DATA_ARRAY_H_
+
+/* Size of the Data Set */
+#define SIZE                (40)
+
+unsigned char numbers[SIZE] = {
+
+    34,   201,  190,  154,  8,    194,  2,    6,
+    114,  88,   45,   76,   123,  87,   25,   23,
+    200,  122,  150,  90,   92,   87,   177,  244,
+    201,  6,    12,   60,   8,    2,    5,    67,
+    7,    87,   250,  230,  99,   3,    100,  90
+                 
+};
+
+#endif /* _DATA_ARRAY_H_ */
