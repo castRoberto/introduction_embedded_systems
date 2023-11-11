82c87054bb6e91b2e170da71e2b9294737609a47
RobertoCastro <robertoca07@hotmail.com>
2023-11-11 18:42:29 -0500

    A utility module is added

---
+++ week1/statistic_project/util/commom.h
@@ -0,0 +1,22 @@
+/********************************************************************************
+ * @file common.h
+ * @brief Utilities for the entire project
+ *
+ * This package contains the implementation of the transversal functionalities 
+ * of the entire project.
+ *
+ * @author Roberto Castro Beltran
+ * @date 08 november 2023
+ *
+ *******************************************************************************/
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl;
+
+#define LOG_ERR(x) std::cerr << x << std::endl;
+
+#endif /* _COMMON_H_ */
