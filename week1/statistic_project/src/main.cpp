11a2d3d1c3ec6de6f3144a9591549abe1e9c0daa
RobertoCastro <robertoca07@hotmail.com>
2023-11-11 18:53:18 -0500

    A module is added with the main function of the program

---
+++ week1/statistic_project/src/main.cpp
@@ -0,0 +1,17 @@
+/********************************************************************************
+ * File: main.cpp
+ * 
+ * Copyright 2023 Roberto Enrique Castro Beltran
+ * 
+ * Author: Roberto Castro
+ * Date: Editted november 2023
+ * 
+ * Description: File with the entry point of the statistics executable. Contains 
+ * the necessary library inclusions for calculations.
+ *******************************************************************************/
+
+
+int main (int argc, char* argv []) {
+
+    return 0;
+}
