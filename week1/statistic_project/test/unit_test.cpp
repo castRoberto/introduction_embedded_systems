ebb0e34f9574ce242804d2e1c657163513a1a6f7
RobertoCastro <robertoca07@hotmail.com>
2023-11-11 18:51:22 -0500

    A test module is added

---
+++ week1/statistic_project/test/unit_test.cpp
@@ -0,0 +1,28 @@
+/********************************************************************************
+ * @file stats.h
+ * @brief Unit tests
+ *
+ * In this package all the unit tests of the system are implemented.
+ *
+ * @author Roberto Castro Beltran
+ * @date 08 november 2023
+ *
+ *******************************************************************************/
+
+#include <iostream>
+
+#include "../util/common.h"
+#include "../data/data_array.h"
+
+int main (int argc, char* argv[]) {
+
+    try {
+
+        throw "No tests implemented!";
+
+    } catch(const char* error) {
+        LOG_ERR(error);
+    }
+
+    return 0;
+}
