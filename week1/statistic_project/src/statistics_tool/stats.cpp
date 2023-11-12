76a2593f46c6df3d3d0c3f6a0ce993b02c85435a
RobertoCastro <robertoca07@hotmail.com>
2023-11-11 19:01:11 -0500

    The module is added with the feature of the program without implementing

---
+++ week1/statistic_project/src/statistics_tool/stats.cpp
@@ -0,0 +1,51 @@
+/********************************************************************************
+ * @file stats.cpp
+ * @brief Implementation of statistical operations
+ *
+ * Implementation of statistical functions on a matrix.
+ *
+ * @author Roberto Castro Beltran
+ * @date 08 november 2023
+ *
+ *******************************************************************************/
+
+#include "stats.h"
+
+
+int find_median (unsigned char* ptr, int len) {
+
+    return NULL;
+}
+
+
+int find_mean (unsigned char* ptr, int len) {
+
+    return NULL;
+
+}
+
+
+int find_maximum (unsigned char* ptr, int len) {
+
+    return NULL;
+}
+
+
+int find_minimum (unsigned char* ptr, int len) {
+
+    return NULL;
+}
+
+
+void sort_array (unsigned char* ptr, int len) {
+
+}
+
+
+void print_array (unsigned char* ptr, int len) {
+
+}
+
+void print_statistics (unsigned char* ptr, int len) {
+    
+}
